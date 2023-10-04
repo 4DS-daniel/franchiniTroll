42bf97aad8e7b7c53d125beacc6fd41aae332a64
Daniel Franchini <73396382+dani3lfrenc@users.noreply.github.com>
2023-10-04 17:12:13 +0200

    feat: exit from fight

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Troll troll = new Troll();
         Random rand = new Random();
         float life_player = 500;
+        Scanner input = new Scanner(System.in);
+        int flag = 1;
         System.out.println("Benvenuto guerriero nella terra dei Goblin, combatti per sopravvivere!");
 
         do {
@@ -13,11 +16,16 @@ public class Main {
             troll.setHp(troll.getHp()-damage);
             System.out.println("- Vita del Troll: " + troll.getHp());
 
+            flag = input.nextInt();
             damage = troll.attack();
             System.out.println("Il Troll attacca: " + damage);
             life_player = life_player - damage;
             System.out.println("- Vita del Giocatore: " + life_player);
-
-        } while (troll.getHp() > 0 && life_player > 0);
+            System.out.println("Vuoi contiuare il combattimento? Premi 1 per continuare o 2 per uscire: \nScelta: ");
+            flag = input.nextInt();
+            if (flag == 2) {
+                System.out.println("Uscira combattimento!");
+            }
+        } while (troll.getHp() > 0 && life_player > 0 && flag != 2);
     }
 }
